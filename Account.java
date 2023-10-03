584f25d601965a4ba3c6ff90428acb409c84c634
Laggstar <ksjolly5@gmail.com>
2023-10-03 08:38:37 -0400

    fixed up private variables and fixed methods in Account.java

--- Account.java
+++ Account.java
@@ -1,33 +1,31 @@
 public class Account {
-    private Account bro;
     private String name;
     private String email;
-    private int gender;
-    private int alien_number, id_in_system, status;
-    public boolean danielStupid = true;
+    private String gender;
+    private String alien_number, id_in_system, status;
     
-    public void CreateNewAc(String[] information){
-	this.bro = new Account();
-	this.bro.name = information[0];
-	this.bro.email = information[1];
-	this.bro.gender = information[2];
-	this.bro.alien_number = information[3];
-	this.bro.id_in_system = information[4];
-	this.bro.status = information[5];
+    public void CreateNewAc(String[] information) {
+	    this.bro.name = information[0];
+	    this.bro.email = information[1];
+	    this.bro.gender = information[2];
+    	this.bro.alien_number = information[3];
+	    this.bro.id_in_system = information[4];
+	    this.bro.status = information[5];
     }
 
-    public Account GetAc() {
-        return this.bro;
+    public Account GetAcc() {
+        return this;
     }
 
     public String ValidateAc(Account account) {
-        return null;
+        return this.alien_number;
     }
     
     public void setName(String newName) {
-	this.name = newName;
+	    this.name = newName;
     }
+
     public int getGender() {
-	    return this.gender;
+	    return String.paraseInteger(this.gender);
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # CS-321-Project
+## Group Members
+- Bryce Roe
+- Daniel Jagga
+- Kabir Jolly
+## Workflow Steps
+- Data Entry (Kabir)
+- Review (Daniel)
+- Approval (Bryce)
