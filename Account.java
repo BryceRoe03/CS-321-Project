0e53ce8cff89b17adceb9091a38fe7473a6fc556
BryceRoe03 <brickroe03@gmail.com>
2023-10-03 08:16:29 -0400

    Account.java

---
+++ Account.java
@@ -0,0 +1,19 @@
+public class Account {
+    private Account bro;
+    private String[] name;
+    private String[] email;
+    private int gender;
+    private int alien_number, id_in_system, status;
+    
+    public void CreateNewAc(String[] information){
+
+    }
+
+    public Account GetAc() {
+        return this.bro;
+    }
+
+    public String[] ValidateAc(Account account) {
+        return null;
+    }
+}
